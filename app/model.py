4ffe0bf91907a2879f37ba9c00b3dbe172235c72
Satyavardhan Koyalkar <satyavardhan24105@gmail.com>
2026-02-24 21:58:55 +0530

    FINAL FIX: tf.keras loader + remove keras conflict

--- app/model.py
+++ app/model.py
@@ -7,14 +7,17 @@ labels = None
 
 def load_once():
     global model, labels
+
     if model is None:
-        print("🌿 Loading .h5 model...")
+        print("🌿 Loading TF model...")
         model = tf.keras.models.load_model("plant_model.h5", compile=False)
+
         with open("labels.json") as f:
             labels = json.load(f)
 
 def predict(arr):
     load_once()
+
     preds = model.predict(arr, verbose=0)[0]
     idx = int(np.argmax(preds))
     return labels[idx], float(preds[idx] * 100)
