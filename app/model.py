2511cafa149a1d606895071fb8d676baf14db1ab
Satyavardhan Koyalkar <satyavardhan24105@gmail.com>
2026-02-24 20:56:43 +0530

    switch to keras v3 compatible model

--- app/model.py
+++ app/model.py
@@ -1,18 +1,28 @@
 import numpy as np
 import json
 from keras.models import load_model
+from pathlib import Path
 
 model = None
 labels = None
+
 
 def load_once():
     global model, labels
 
     if model is None:
-        print("🌿 Loading model (legacy mode)...")
-        model = load_model("plant_model.h5", compile=False)
+        print("🌿 Loading model (.keras format)...")
 
-        with open("labels.json") as f:
+        # Resolve absolute paths (Render safe)
+        base = Path(__file__).resolve().parent.parent
+        model_path = base / "plant_model_fixed.keras"
+        labels_path = base / "labels.json"
+
+        # Load model
+        model = load_model(model_path, compile=False)
+
+        # Load labels
+        with open(labels_path) as f:
             labels = json.load(f)
 
 
